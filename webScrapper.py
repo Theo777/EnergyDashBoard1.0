fa8b5e1f2ca8d40d69128a543896461e8899eff5
Theo777 <theoslepski@yahoo.com>
2018-09-26 23:50:53 -0400

    Made outline for parsing CSV file

--- webScrapper.py
+++ webScrapper.py
@@ -1,3 +1,5 @@
+import csv
+
 def main():
     arrOfBuildingNames,arrOfIpAdresses=readInCSV()
     #SCRAPE WEB HERE
@@ -5,6 +7,11 @@ def main():
 def readInCSV():
     arrOfBuildingNames = []
     arrOfIpAdresses=[]
+    with open('Meter_IP_addresses.csv',mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+
     return arrOfIpAdresses,arrOfBuildingNames
 
 if __name__ == '__main__':
