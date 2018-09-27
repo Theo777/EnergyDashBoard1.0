74bad5e4c150ffc392cc345b5c7ea956c7b11bb3
Theo777 <theoslepski@yahoo.com>
2018-09-26 22:56:24 -0400

    Made Web Scrapper/Initial commit

---
+++ webScrapper.py
@@ -0,0 +1,11 @@
+def main():
+    arrOfBuildingNames,arrOfIpAdresses=readInCSV()
+    #SCRAPE WEB HERE
+
+def readInCSV():
+    arrOfBuildingNames = []
+    arrOfIpAdresses=[]
+    return arrOfIpAdresses,arrOfBuildingNames
+
+if __name__ == '__main__':
+    main()
