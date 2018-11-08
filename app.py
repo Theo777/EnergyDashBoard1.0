19cfd07e8624c82ebdb43f20985ead56461c9d8e
Theo777 <theoslepski@yahoo.com>
2018-11-07 20:46:34 -0500

    Simple flask program that reads the dao method that is currently being worked on and displays it on a random table.

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['POST', 'GET'])
+def squareOne():
+    dao=dao()
+    info =dao.getInfo()
+
+    if(request.method=='POST'):
+        return render_template('home.html', **locals())
+    elif(request.method=='GET'):
+        return render_template('home.html', **locals())
 
 
 if __name__ == '__main__':
