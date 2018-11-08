007781d297967a97facafe6a02909293ec82db49
Alexander Avery <apavery@protonmail.com>
2018-11-07 20:56:41 -0500

    added models for database

---
+++ model/Meter.py
@@ -0,0 +1,8 @@
+class Meter():
+
+    def __init__(self, buildingName, subBuildings, heatingType, residents):
+        self.buildingName = buildingName
+        self.subBuildings = subBuildings
+        self.heatingType = heatingType
+        self.residents = residents
+    
---
+++ model/MeterReading.py
@@ -0,0 +1,9 @@
+import datetime
+
+class MeterReading():
+
+    def __init__(self, buildingName, value, timeStamp):
+        self.buildingName = buildingName
+        self.value = value
+        self.timeStamp = timeStamp
+    
