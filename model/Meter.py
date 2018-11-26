587c9e03fc9369c0a03c306b73a9b0dc639e403d
Alexander Avery <apavery@protonmail.com>
2018-11-25 19:54:15 -0500

    implemented __add__ in MeterReading.py

--- model/Meter.py
+++ model/Meter.py
@@ -5,4 +5,3 @@ class Meter():
         self.subBuildings = subBuildings
         self.heatingType = heatingType
         self.residents = residents
-    
--- model/MeterReading.py
+++ model/MeterReading.py
@@ -6,4 +6,6 @@ class MeterReading():
         self.buildingName = buildingName
         self.value = value
         self.timeStamp = timeStamp
-    
+
+    def __add__(self, other):
+        return self.value + other.value
