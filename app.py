f9e432fb4d2e3d1161612af81f990b3afb8c9891
Alexander Avery <apavery@protonmail.com>
2019-02-05 21:48:19 -0500

    added gitignore, fixed dao, modified home.html wording

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from dao import Dao
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def squareOne():
-    dao=dao()
-    info =dao.getInfo()
+    dao=Dao()
+    info=dao.getAllDocumets()
 
     if(request.method=='POST'):
-        return render_template('home.html', **locals())
+        return render_template('home.html', info=info)
     elif(request.method=='GET'):
-        return render_template('home.html', **locals())
+        return render_template('home.html', info=info)
 
 
 if __name__ == '__main__':
--- dao.py
+++ dao.py
@@ -6,19 +6,27 @@ class Dao():
     def __init__(self, database_ip='localhost', port=27017, database='MeterData', collection='MeterReadings'):
         client = MongoClient(database_ip, port)
         self.db = client[database]
-        self.collection = db[collection]
+        self.collection = self.db[collection]
 
     #Get documents by building name
-    def getDocumentsByName(self, _buildingName, collection=self.collection):
+    def getDocumentsByName(self, _buildingName):
         # return list of MeterReading by building name
-        return collection.find({"buildingName": _name})
+        documents = self.collection.find({"buildingName": _buildingName})
+        return [document for document in documents]
 
     #Get documents by heating type
     def getDocumentsByHeating(self, _heatingType):
         # return a list of meter readings by heating type
-        return self.collection.find({"heatingType": _heatingType})
+        document = self.collection.find({"heatingType": _heatingType})
+        return [document for document in documents]
 
     #Get documents by time
     def getDocumentsAfterTimeStamp(self, _timeStamp):
         # return list of MeterReading after a specific timestamp
-        return self.collection.find({"timeStamp": {"$lt": _timeStamp}})
+        documents = self.collection.find({"timeStamp": {"$lt": _timeStamp}})
+        return [document for document in documents]
+
+    def getAllDocumets(self):
+        # return a list of all documents in collection
+        documents = self.collection.find({})
+        return [document for document in documents]
