24e77eaaad0276620332ddc2ff70596d71757a0a
Neil <neilfitzy2y@gmail.com>
2019-03-27 20:57:21 -0400

    Pushing test code for java script chart

--- static/ChartJavaScript.js
+++ static/ChartJavaScript.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+window.onload =function() {
     var years = [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050];
 // For drawing the lines
     var africa = [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478];
@@ -20,4 +20,4 @@ $( document ).ready(function() {
         }
     })
 
-});
+};
