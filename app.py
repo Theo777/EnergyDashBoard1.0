c75e4ace1129d1039159bc13359816d86f3ea3f3
Alexander Avery <apavery@protonmail.com>
2019-04-10 20:55:20 -0400

    added reading json route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from dao import Dao
 
 app = Flask(__name__)
@@ -14,6 +14,15 @@ def squareOne():
         return render_template('home.html', info=info)
     elif(request.method=='GET'):
         return render_template('home.html', info=info)
+
+@app.route('/readings', methods=['POST', 'GET'])
+def readings():
+    dao=Dao()
+    if(request.method=='POST'):
+        _name = request.form.get('buildingName')
+        info=dao.getDocumentsByName(_name)
+        json_readings = [reading.to_json() for reading in info]
+        return jsonify(json_readings)
 
 
 if __name__ == '__main__':
--- model/meter_reading.py
+++ model/meter_reading.py
@@ -7,6 +7,9 @@ class MeterReading():
         self.value = value
         self.timeStamp = timeStamp
 
+    def to_json(self):
+        return {'buildingName': self.buildingName, 'value': self.value, 'timeStamp': self.timeStamp.strftime('%Y-%m-%d')}
+
     @classmethod
     def from_dict(cls, data):
         return cls(data.get('buildingName'), data.get('value'), data.get('timeStamp'))
